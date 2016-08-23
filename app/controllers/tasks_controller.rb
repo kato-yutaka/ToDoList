e3ff953d886ace62580a722b13296032e1fefaf5
katoyutaka <kato-yutaka@outlook.jp>
2016-08-24 01:44:56 +0900

    password２回認証

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -81,6 +81,7 @@ end
 
 
   private
+
     # Use callbacks to share common setup or constraints between actions.
     def set_task
       @task = Task.find(params[:id])
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,9 @@
 class User < ActiveRecord::Base
  has_many :tasks
+
+
+ 
+ validates_confirmation_of :password
+ attr_accessor             :password_confirmation
+ 
 end
